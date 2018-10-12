65a54d15ab816e5e522a70664030ccceb021971e
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-10-12 01:03:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  var newObje = Object.assign({})
+  var newObj = Object.assign({}, object)
 }
 
 function destructivelyDeleteFromObjectByKey(object, key){
